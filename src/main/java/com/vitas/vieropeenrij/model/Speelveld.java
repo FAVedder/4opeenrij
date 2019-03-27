8d313177fe8581659d2d60075a2d47d9761c2318
FAVedder <filip.vedder@gmail.com>
2019-03-27 12:18:20 +0100

    Array aangepast naar iets dat beter werkt en een method toegevoegd om het veld leeg te gooien

--- src/main/java/com/vitas/vieropeenrij/model/Speelveld.java
+++ src/main/java/com/vitas/vieropeenrij/model/Speelveld.java
@@ -2,31 +2,36 @@ package com.vitas.vieropeenrij.model;
 
 public class Speelveld {
     
-    private char[][] speelVeld;
+    private char[][] speelVeld = new char[6][7];
 
-    public Speelveld() {
-        speelVeld = new char[7][6];
-    }
-    
     public char[][] getSpeelVeld() {
         return speelVeld;
     }
 
     public void setSpeelVeld(char[][] speelVeld) {
         //check of het veld de juiste dimensies heeft
-        if(speelVeld.length == 7) {
+        if(speelVeld.length == 6) {
             for(int i = 1; i < speelVeld.length; i++) {
-                if(speelVeld[i].length != 6) {
+                if(speelVeld[i].length != 7) {
                     System.out.println("Error");
-                    this.speelVeld = new char[7][6];
+                    this.speelVeld = new char[6][7];
                 } else {
                     this.speelVeld = speelVeld;
                 }
             }    
         } else {
             System.out.println("error");
-            this.speelVeld = new char[7][6];
+            this.speelVeld = new char[6][7];
         }    
+    }
+    
+    public void clearSpeelVeld() {
+        for(int i = 0; i <= speelVeld.length - 1; i++) {
+            for(int y = 0; y <=speelVeld[i].length - 1; y++) {
+                speelVeld[i][y] = '-';
+            }
+        }
+            
     }
     
 }
