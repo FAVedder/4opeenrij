10522857905a0cca675c08958bcd7f9167fcf498
FAVedder <filip.vedder@gmail.com>
2019-03-27 12:18:45 +0100

    Score variabele toegevoegd om aantal gewonnen spellen bij te houden

--- src/main/java/com/vitas/vieropeenrij/model/Speler.java
+++ src/main/java/com/vitas/vieropeenrij/model/Speler.java
@@ -3,6 +3,7 @@ package com.vitas.vieropeenrij.model;
 public class Speler {
     
     private String naam;
+    private int score;
 
     public String getNaam() {
         return naam;
@@ -11,5 +12,14 @@ public class Speler {
     public void setNaam(String naam) {
         this.naam = naam;
     }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
     
+        
 }
