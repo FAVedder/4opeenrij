b81f566883460a8b4d4d6b7740632fb74fd05698
FAVedder <filip.vedder@gmail.com>
2019-03-27 17:41:58 +0100

    Extreem hippe ASCI art toegevoegd

--- src/main/java/com/vitas/vieropeenrij/view/HoofdMenu.java
+++ src/main/java/com/vitas/vieropeenrij/view/HoofdMenu.java
@@ -8,7 +8,14 @@ public class HoofdMenu {
     private int optie;
     
     public void menu() {
-        System.out.println("4 op een rij");
+        System.out.println(" _   _ _                                                  _ _ ");
+        System.out.println("| | | (_)                                                (_|_)");
+        System.out.println("| | | |_  ___ _ __    ___  _ __     ___  ___ _ __    _ __ _ _ ");
+        System.out.println("| | | | |/ _ \\ '__|  / _ \\| '_ \\   / _ \\/ _ \\ '_ \\  | '__| | |");
+        System.out.println("\\ \\_/ / |  __/ |    | (_) | |_) | |  __/  __/ | | | | |  | | |");
+        System.out.println(" \\___/|_|\\___|_|     \\___/| .__/   \\___|\\___|_| |_| |_|  |_| |");
+        System.out.println("                          | |                             _/ |");
+        System.out.println("                          |_|                            |__/");
         System.out.println("Kies een optie:");
         System.out.println("1. Nieuw spel");
         System.out.println("2. Stoppen");
