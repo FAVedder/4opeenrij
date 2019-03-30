bb2b401b6aa0d1118563b23f2214df4c5175535d
Filip Vedder <filip.vedder@gmail.com>
2019-03-30 13:18:24 +0100

    maakSpeler methode van view naar controller verplaatst

--- src/main/java/com/vitas/vieropeenrij/controller/NieuwSpelMenuController.java
+++ src/main/java/com/vitas/vieropeenrij/controller/NieuwSpelMenuController.java
@@ -7,18 +7,35 @@ import com.vitas.vieropeenrij.view.NieuwSpelMenu;
 public class NieuwSpelMenuController {
 
     private NieuwSpelMenu nieuwSpelMenu = new NieuwSpelMenu();
+    private SpelController spelController = new SpelController();
+    
     private Speler speler1;
     private Speler speler2;
-    private SpelController spelController = new SpelController();
+    
+    private String spelerNaam1;
+    private String spelerNaam2;
     
     public void start() {
         nieuwSpelMenu.menu();
-        speler1 = nieuwSpelMenu.getSpeler1();
-        speler2 = nieuwSpelMenu.getSpeler2();
+        spelerNaam1 = nieuwSpelMenu.getSpelerNaam1();
+        spelerNaam2 = nieuwSpelMenu.getSpelerNaam2();
+        maakSpelers(spelerNaam1, spelerNaam2);
         Speelveld speelveld = new Speelveld();
         speelveld.clearSpeelVeld();
         speelveld.setBeurt(Speelveld.Beurt.SPELER1);
         spelController.start(speler1, speler2, speelveld);
+    }
+    
+    public void maakSpelers(String spelerNaam1, String spelerNaam2) {
+        speler1 = new Speler();
+        speler1.setNaam(spelerNaam1);
+        speler1.setScore(0);
+        speler1.setBeurten(0);
+        
+        speler2 = new Speler();
+        speler2.setNaam(spelerNaam2);
+        speler2.setScore(0);
+        speler2.setBeurten(0);
     }
     
 }
--- src/main/java/com/vitas/vieropeenrij/view/NieuwSpelMenu.java
+++ src/main/java/com/vitas/vieropeenrij/view/NieuwSpelMenu.java
@@ -1,15 +1,12 @@
 package com.vitas.vieropeenrij.view;
 
-import com.vitas.vieropeenrij.model.Speler;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class NieuwSpelMenu {
 
-    private Speler speler1;
-    private Speler speler2;
-    String spelerNaam1;
-    String spelerNaam2;
+    private String spelerNaam1;
+    private String spelerNaam2;
     
     public void menu() {
         Scanner in = new Scanner(System.in);
@@ -19,35 +16,18 @@ public class NieuwSpelMenu {
         
             System.out.print("Voer naam speler 2 in: ");
             spelerNaam2 = in.nextLine();
-        
-            maakSpelers(spelerNaam1, spelerNaam2);
-        } 
-        catch(InputMismatchException e) {
+        } catch (InputMismatchException e) {
             System.out.println("Voer een geldige naam in.");
             menu();
         }
     }
     
-    public void maakSpelers(String spelerNaam1, String spelerNaam2) {
-        speler1 = new Speler();
-        speler1.setNaam(spelerNaam1);
-        speler1.setScore(0);
-        speler1.setBeurten(0);
-        
-        speler2 = new Speler();
-        speler2.setNaam(spelerNaam2);
-        speler2.setScore(0);
-        speler2.setBeurten(0);
+    public String getSpelerNaam1() {
+        return spelerNaam1;
     }
 
-    public Speler getSpeler1() {
-        return speler1;
+    public String getSpelerNaam2() {
+        return spelerNaam2;
     }
-
-    public Speler getSpeler2() {
-        return speler2;
-    }
-    
-    
     
 }
