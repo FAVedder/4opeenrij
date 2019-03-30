3ae0e0348db3bb4b2422ab2b69c90bfdd01b9ffb
Filip Vedder <filip.vedder@gmail.com>
2019-03-30 13:58:02 +0100

    Constructor toegevoegd

--- src/main/java/com/vitas/vieropeenrij/model/Game.java
+++ src/main/java/com/vitas/vieropeenrij/model/Game.java
@@ -5,7 +5,15 @@ public class Game {
     private Speler speler1;
     private Speler speler2;
     private Speelveld speelveld;
+    
+    private String gameName;
 
+    public Game(Speler speler1, Speler speler2, Speelveld speelveld) {
+        this.speler1 = speler1;
+        this.speler2 = speler2;
+        this.speelveld = speelveld;
+    }
+    
     public Speler getSpeler1() {
         return speler1;
     }
@@ -28,6 +36,14 @@ public class Game {
 
     public void setSpeelveld(Speelveld speelveld) {
         this.speelveld = speelveld;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public void setGameName(String gameName) {
+        this.gameName = gameName;
     }
     
 }
